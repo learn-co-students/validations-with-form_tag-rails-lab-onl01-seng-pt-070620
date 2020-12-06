c7eca2d45e00e2dd2d6e316f2df23673be3b775e
Jbeltrez <beltrezjohn@gmail.com>
2020-12-05 23:51:30 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # get "/authors/new", to: "authors#new", as: "new_author"
+  resources :authors, only: [:index, :show, :new, :create, :edit, :update]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
